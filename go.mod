ec24d99371b5ecae4f451495d6a46d841988448e
Ray Antonius <rayantonius@gmail.com>
2021-07-03 09:19:01 +0700

    update regex example

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/Rocksus/mock-matcher
 
 go 1.16
-
-require github.com/google/uuid v1.2.0
--- match_regex_test.go
+++ match_regex_test.go
@@ -2,25 +2,17 @@ package matcher
 
 import (
 	"testing"
-
-	"github.com/google/uuid"
 )
 
 func Test_regexMatcher_Matches(t *testing.T) {
 	pattern := "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"
 	matcher := NewRegexMatcher(pattern)
 
-	// generate UUID v4
-	data, err := uuid.NewRandom()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if res := matcher.Matches(data.String()); !res {
+	if res := matcher.Matches("51e5fe10-5325-4a32-bce8-7ebe9708c453"); !res {
 		t.Errorf("failure, expected true")
 	}
 
-	if res := matcher.Matches("a123av"); res {
+	if res := matcher.Matches("abcdefgh-ijkl-mnop-qrst-uvwxyzabcdef"); res {
 		t.Errorf("failure, expected false")
 	}
 }
